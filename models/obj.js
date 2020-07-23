4a7291b3d9d81ad53d2145231e7e6a560a5bc26e
toanhminh0412 <55383509+toanhminh0412@users.noreply.github.com>
2020-07-22 18:24:00 -0700

    Folder updated

--- models/obj.js
+++ models/obj.js
@@ -13,14 +13,20 @@ class TeamComp {
     }
 
     teamcompScore() {
+        // Calculate teamfight score of the comp 
         this.teamFightScore();
+        // Calculate pick score of the comp 
         this.pickScore();
+        // Calculate hyper score of the comp 
         this.hyperScore();
+        // Calculate poke score of the comp 
         this.pokeScore();
+        // Calculate split score of the comp 
         this.splitScore();
     }
 
     teamFightScore() {
+        // Variables declaration
         let teamfightScore = 0;
         let vanFound = false;
         let divFound = false;
@@ -28,41 +34,57 @@ class TeamComp {
         let burFound = false;
         let batFound = false;
         let marFound = false;
+        // Loop through the champions of the comp
         this.champions.forEach(champion => {
             // console.log(champion.teamcomp.Teamfight);
+            // Add teamfight score of a single champion to the total score
             teamfightScore += champion.teamcomp.Teamfight;
+
+            // Check if the comp has a vanguard (S tier)
             if (champion.tags[0] === 'van' && vanFound === false) {
                 teamfightScore += 20;
                 vanFound = true;
             }
+
+            // Check if the comp has a burst (S tier) 
             if (champion.tags[0] === 'bur' && burFound === false) {
                 teamfightScore += 20;
                 burFound = true;
             }
+
             if (champion.tags[1] === 'van' && vanFound === false) {
                 teamfightScore += 10;
                 vanFound = true;
             }
+
             if (champion.tags[1] === 'bur' && burFound === false) {
                 teamfightScore += 10;
                 burFound = true;
             }
+
+            // Check if the comp has a diver (A tier)
             if (champion.tags[0] === 'div' && divFound === false) {
                 teamfightScore += 10;
                 divFound = true;
             }
+
+            // Check if the comp has a artillery (A tier) 
             if (champion.tags[0] === 'art' && artFound === false) {
                 teamfightScore += 10;
                 artFound = true;
             }
+
+            // Check if the comp has a battle mage (A tier)
             if (champion.tags[0] === 'bat' && batFound === false) {
                 teamfightScore += 10;
                 batFound = true;
             }
+
             if (champion.tags[1] === 'div' && divFound === false) {
                 teamfightScore += 5;
                 divFound = true;
             }
+
             if (champion.tags[1] === 'art' && artFound === false) {
                 teamfightScore += 5;
                 artFound = true;
@@ -71,6 +93,8 @@ class TeamComp {
                 teamfightScore += 5;
                 batFound = true;
             }
+
+            // Check if the comp has a marksman (S tier)
             if (champion.tags[0] === 'mar' && marFound === false) {
                 teamfightScore += 20;
                 marFound = true;
@@ -80,6 +104,8 @@ class TeamComp {
     }
 
     pickScore() {
+
+        // Variable Declarations
         let score = 0;
         let assFound = false;
         let divFound = false;
@@ -87,8 +113,14 @@ class TeamComp {
         let catFound = false;
         let burFound = false;
         let marFound = false;
+
+        // Loop through every champion in the comp
         this.champions.forEach(champion => {
+
+            // Add pick score of each champion to the total pick score
             score += champion.teamcomp.Pick;
+
+            // Check for assasin (S tier)
             if (champion.tags[0] === 'ass' && assFound === false) {
                 score += 20;
                 assFound = true;
@@ -97,6 +129,8 @@ class TeamComp {
                 score += 10;
                 assFound = true;
             }
+
+            // Check for diver (S tier)
             if (champion.tags[0] === 'div' && divFound === false) {
                 score += 20;
                 divFound = true;
@@ -105,6 +139,8 @@ class TeamComp {
                 score += 10;
                 divFound = true;
             }
+
+            // Check for vanguard (A tier)
             if (champion.tags[0] === 'van' && vanFound === false) {
                 score += 10;
                 vanFound = true;
@@ -113,6 +149,8 @@ class TeamComp {
                 score += 5;
                 vanFound = true;
             }
+
+            // Check for catcher (S tier)
             if (champion.tags[0] === 'cat' && catFound === false) {
                 score += 20;
                 catFound = true;
@@ -121,6 +159,8 @@ class TeamComp {
                 score += 10;
                 catFound = true;
             }
+
+            // Check for burst (S tier)
             if (champion.tags[0] === 'bur' && burFound === false) {
                 score += 20;
                 burFound = true;
@@ -129,6 +169,8 @@ class TeamComp {
                 score += 10;
                 burFound = true;
             }
+
+            // Check for marksman (S tier)
             if (champion.tags[0] === 'mar' && marFound === false) {
                 score += 10;
                 marFound = true;
@@ -138,14 +180,22 @@ class TeamComp {
     }
 
     hyperScore() {
+
+        // Variables Declaration
         let hScore = 0;
         let jugFound = false;
         let warFound = false;
         let encFound = false;
         let batFound = false;
         let marFound = false;
+
+        // Loop through every champion in the comp
         this.champions.forEach(champion => {
+
+            // Add hyper carry score of each champion to the total hyper carry score
             hScore += champion.teamcomp.Hyper;
+
+            // Check for juggernaut (A tier)
             if (champion.tags[0] === 'jug' && jugFound === false) {
                 hScore += 10;
                 jugFound = true;
@@ -154,6 +204,8 @@ class TeamComp {
                 hScore += 5;
                 jugFound = true;
             }
+
+            // Check for warden (S tier)
             if (champion.tags[0] === 'war' && warFound === false) {
                 hScore += 20;
                 warFound = true;
@@ -162,6 +214,8 @@ class TeamComp {
                 hScore += 10;
                 warFound = true;
             }
+
+            // Check for enchanter (S tier)
             if (champion.tags[0] === 'enc' && encFound === false) {
                 hScore += 20;
                 encFound = true;
@@ -170,6 +224,8 @@ class TeamComp {
                 hScore += 10;
                 encFound = true;
             }
+
+            // Check for battle mage (A tier)
             if (champion.tags[0] === 'bat' && batFound === false) {
                 hScore += 10;
                 batFound = true;
@@ -178,6 +234,8 @@ class TeamComp {
                 hScore += 5;
                 batFound = true;
             }
+
+            // Check for marksman (S tier)
             if (champion.tags[0] === 'mar' && marFound === false) {
                 hScore += 20;
                 marFound = true;
@@ -188,6 +246,8 @@ class TeamComp {
     }
 
     pokeScore() {
+
+        // Variables declaration
         let pScore = 0;
         let warFound = false;
         let catFound = false;
@@ -196,8 +256,14 @@ class TeamComp {
         let burFound = false;
         let batFound = false;
         let marFound = false;
+
+        // Loop through every champion in the comp
         this.champions.forEach(champion => {
+
+            // Add poke score of each championg to the total poke score
             pScore += champion.teamcomp.Poke;
+
+            // Check for warden (S tier)
             if (champion.tags[0] === 'war' && warFound === false) {
                 pScore += 20;
                 warFound = true;
@@ -206,6 +272,7 @@ class TeamComp {
                 pScore += 10;
                 warFound = true;
             }
+            // Check for catcher (A tier)
             if (champion.tags[0] === 'cat' && catFound === false) {
                 pScore += 10;
                 catFound = true;
@@ -214,6 +281,8 @@ class TeamComp {
                 pScore += 5;
                 catFound = true;
             }
+
+            // Check for enchanter (S tier)
             if (champion.tags[0] === 'enc' && encFound === false) {
                 pScore += 20;
                 encFound = true;
@@ -222,6 +291,8 @@ class TeamComp {
                 pScore += 10;
                 encFound = true;
             }
+
+            // Check for artillery (S tier)
             if (champion.tags[0] === 'art' && artFound === false) {
                 pScore += 20;
                 artFound = true;
@@ -230,6 +301,8 @@ class TeamComp {
                 pScore += 10;
                 artFound = true;
             }
+
+            // Check for burst (A tier)
             if (champion.tags[0] === 'bur' && burFound === false) {
                 pScore += 10;
                 burFound = true;
@@ -238,6 +311,8 @@ class TeamComp {
                 pScore += 5;
                 burFound = true;
             }
+
+            // Check for battle mage (A tier)
             if (champion.tags[0] === 'bat' && batFound === false) {
                 pScore += 10;
                 burFound = true;
@@ -246,6 +321,8 @@ class TeamComp {
                 pScore += 5;
                 burFound = true;
             }
+
+            // Check for marksman (A tier)
             if (champion.tags[0] === 'mar' && marFound === false) {
                 pScore += 10;
                 marFound = true;
@@ -255,6 +332,8 @@ class TeamComp {
     }
 
     splitScore() {
+
+        // Variables Declaration 
         let sScore = 0;
         let assFound = false;
         let skiFound = false;
@@ -262,8 +341,14 @@ class TeamComp {
         let warFound = false;
         let encFound = false;
         let marFound = false;
+
+        // Loop through every champions in the comp
         this.champions.forEach(champion => {
+
+            // Add the split score of each champion to the total split score
             sScore += champion.teamcomp.Split;
+
+            // check for assasin (A tier)
             if (champion.tags[0] === 'ass' && assFound === false) {
                 sScore += 10;
                 assFound = true;
@@ -272,6 +357,8 @@ class TeamComp {
                 sScore += 5;
                 assFound = true;
             }
+
+            // check for skimisher (S tier)
             if (champion.tags[0] === 'ski' && skiFound === false) {
                 sScore += 20;
                 skiFound = true;
@@ -280,6 +367,8 @@ class TeamComp {
                 sScore += 10;
                 skiFound = true;
             }
+
+            // check for juggernaut (S tier)
             if (champion.tags[0] === 'jug' && jugFound === false) {
                 sScore += 20;
                 jugFound = true;
@@ -288,6 +377,8 @@ class TeamComp {
                 sScore += 10;
                 jugFound = true;
             }
+
+            // check for warden (A tier)
             if (champion.tags[0] === 'war' && warFound === false) {
                 sScore += 10;
                 warFound = true;
@@ -296,6 +387,8 @@ class TeamComp {
                 sScore += 5;
                 warFound = true;
             }
+
+            // check for enchanter (A tier)
             if (champion.tags[0] === 'enc' && encFound === false) {
                 sScore += 10;
                 encFound = true;
@@ -304,6 +397,8 @@ class TeamComp {
                 sScore += 5;
                 encFound = true;
             }
+
+            // check for marksman (A tier)
             if (champion.tags[0] === 'mar' && marFound === false) {
                 sScore += 10;
                 marFound = true;
