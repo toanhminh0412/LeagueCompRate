3103cf18df1671b6ab51e3840939ff895f98a75b
toanhminh0412 <55383509+toanhminh0412@users.noreply.github.com>
2020-08-19 19:03:16 -0700

    Folder updated

--- views/champPickView.js
+++ views/champPickView.js
@@ -7,3 +7,9 @@ export const closeChampBoard = () => {
     document.querySelector('.champ-pick-interface').style.display = 'none';
     document.querySelector('html').style.overflowY = 'visible';
 }
+
+export const recommendInterface = (champions, team) => {
+    document.querySelector(`#${team}-first-rec-pick img`).src = `./img/tiles/${champions[0].img}`;
+    document.querySelector(`#${team}-second-rec-pick img`).src = `./img/tiles/${champions[1].img}`;
+    document.querySelector(`#${team}-third-rec-pick img`).src = `./img/tiles/${champions[2].img}`;
+}
